e4af1aef1c3dea6c201f7523521c0fdd00150850
Lidiya Lesnikova <lesnikoval@yandex.ru>
2024-07-16 21:25:30 +0300

    dz 1 complete

--- Task1/firstapp/views.py
+++ Task1/firstapp/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 import logging
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -5,7 +6,34 @@ from django.http import HttpResponse
 logger = logging.getLogger(__name__)
 
 def index(request):
-    return HttpResponse("My first application")
+    html = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8"/>
+    <title>Start</title>
+    </head>
+    <body>
+    <h1>My first application</h1>
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
 
 def about(request):
-    return HttpResponse("About me")
+    today = date.today()
+    html = f'''
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8"/>
+    <title>About me</title>
+    </head>
+    <body>
+    <h1>About me</h1>
+    <p>today {today}</p><br>
+    The weather is hot. The temperature is +40C'
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
